2a9659ad13c34b7c57263047186fb62c7d71151e
birdsong5 <108277183+birdsong5@users.noreply.github.com>
2023-09-24 17:15:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-router-budget-app
+ expenses manager
---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    loader: dashboardLoader,
+  },
+  {
+    path: "about",
+    element: <h1>About</h1>,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
