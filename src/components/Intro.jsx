ef9d7c91bcd5a51c45fac872b155855d9a7e6b23
birdsong5 <108277183+birdsong5@users.noreply.github.com>
2023-10-08 22:06:33 +0200

    updated Intro component

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,5 +1,36 @@
+import { Form } from "react-router-dom";
+
+//library
+import { UserPlusIcon } from "@heroicons/react/24/solid";
+
 const Intro = () => {
-  return <div>Intro</div>;
+  return (
+    <div className="intro">
+      <div>
+        <h1>
+          Take control of <span className="accent">Your Money</span>
+        </h1>
+        <p>
+          Personal budgeting is the secret to financial freedom. Start your
+          journey today.
+        </p>
+        <Form method="post">
+          <input
+            type="text"
+            name="userName"
+            required
+            placeholder="What is your name?"
+            area-label="Your name"
+            autoComplete="given-name"
+          />
+          <button type="submit" className="btn btn--dark">
+            <span>Create Account</span>
+            <UserPlusIcon width={20} />
+          </button>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
