a92db2253f0e1d2cca0b421d20b4d246821842a0
birdsong5 <108277183+birdsong5@users.noreply.github.com>
2024-05-25 22:35:54 +0200

    created ExpenseItem component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,16 +1,20 @@
+//components
+import ExpenseItem from "./ExpenseItem";
+
 const Table = ({ expenses }) => {
   return (
     <div className="table">
       <table>
         <thead>
-          <tr></tr>
+          <tr>
+            {["Name", "Amount", "Date"].map((i, index) => (
+              <th key={index}>{i}</th>
+            ))}
+          </tr>
         </thead>
         <tbody>
           {expenses.map((expense) => (
-            <tr key={expense.id}>
-              {expense.name}
-              {/*<ExpenseItem />*/}
-            </tr>
+            <tr key={expense.id}>{<ExpenseItem expense={expense} />}</tr>
           ))}
         </tbody>
       </table>
