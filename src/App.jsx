373dbced45918e2ebc0827c8405c2cf0cfb6185a
birdsong5 <108277183+birdsong5@users.noreply.github.com>
2024-06-09 19:29:41 +0200

    updated the route name for BudgetPage

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 //Routes
 import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
-import BudgetPage, { budgetLoader } from "./pages/Budgetpage";
+import BudgetPage, { budgetLoader } from "./pages/BudgetPage";
 import Error from "./pages/Error";
 import ExpensesPage, {
   expensesAction,
